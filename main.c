132f596feac338023911f2e2085296117a306f19
vladeemerr <50291143+vladeemerr@users.noreply.github.com>
2024-03-30 19:10:36 +0300

    Implement flag parsing, introduce verbose mode

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "common.h"
 #include "string.h"
@@ -8,16 +9,51 @@
 #include "lexer.h"
 #include "parser.h"
 
+static bool verbose = false;
+
+static void print_short_usage(void)
+{
+    LOG("Usage: simplify [-h|--help] [-v] {expression}\n");
+    exit(EXIT_SUCCESS);
+}
+
+static void print_long_usage(void)
+{
+    LOG("Usage: simplify [-h|--help] [-v] {expression}\n\n"
+        "\t-h, --help\n"
+        "\t\tOutput a usage message and exit\n\n"
+        "\t-v\n"
+        "\t\tEnable output of debug messages\n");
+    exit(EXIT_SUCCESS);
+}
+
 int main(int argc, char* argv[])
 {
-    if (argc < 2) {
-        LOGF("Usage: %s expression\n", argv[0]);
-        exit(EXIT_SUCCESS);
+    if (argc == 1)
+        print_short_usage();
+
+    int argp = 1;
+    while (argp != argc) {
+        if (strcmp(argv[argp], "-v") == 0) {
+            verbose = true;
+            ++argp;
+        } else if (strcmp(argv[argp], "-h") == 0) {
+            print_short_usage();
+        } else if (strcmp(argv[argp], "--help") == 0)  {
+            print_long_usage();
+        } else
+            break;
     }
 
-    String input = string_init(argv[1]);
+    if (argv[argp] == NULL)
+        print_short_usage();
+
+    String input = string_init(argv[argp]);
     List tokens = lexical_scan(&input);
     Tree tree = parse_expression(&tokens);
+
+    if (verbose)
+        print_tokens(&tokens);
 
     print_expression(&tree);
 
