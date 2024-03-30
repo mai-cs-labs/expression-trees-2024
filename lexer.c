628efcb5ce25740b6605ff7994ad73a9738b6a2d
vladeemerr <50291143+vladeemerr@users.noreply.github.com>
2024-03-30 22:49:34 +0300

    Move `preprocess_tokens` from parser to lexer

--- lexer.c
+++ lexer.c
@@ -28,6 +28,8 @@ static bool lexer_accept(Lexer* const lexer, const String* const valid);
 static void lexer_accept_run(Lexer* const lexer, const String* const valid);
 static void lexer_emit(Lexer* const lexer, const TokenType type);
 
+static bool preprocess_tokens(List* const tokens);
+
 static bool is_digit(const uint8_t c);
 static bool is_operator(const uint8_t c);
 static bool is_letter(const uint8_t c);
@@ -48,6 +50,8 @@ List lexical_scan(const String* const string)
     while (state != NULL) {
         state = (LexerStateFn)state(&lexer);
     }
+
+    preprocess_tokens(&lexer.tokens);
 
     return lexer.tokens;
 }
@@ -279,6 +283,42 @@ static void lexer_emit(Lexer* const lexer, const TokenType type)
     *list_insert_back(&lexer->tokens, Token) = (Token){type, content};
 
     lexer->start = lexer->position;
+}
+
+// When two numbers are together and the second number contains an operator
+// at the beginning of it, then input was {"X", "+Y"} or {"X", "-Y"}, thus
+// an operator token must be synthesised from it and operator symbol cut afterwards
+static bool preprocess_tokens(List* const tokens)
+{
+    assert(tokens != NULL);
+
+    for list_range(it, *tokens) {
+        Token* token = list_node_data(it, Token);
+        ListNodeBase* next_it = it->next;
+
+        if (next_it == NULL)
+            break;
+
+        Token* next = list_node_data(next_it, Token);
+
+        if (token->type == TokenType_number &&
+            next->type == TokenType_number) {
+            const uint8_t op = next->content.text[0];
+
+            if (op == '+' || op == '-') {
+                *list_insert_after(tokens, it, Token) = (Token){
+                    .type = op == '+' ? TokenType_add : TokenType_subtract,
+                    .content = string_trim(&next->content, 0, 1),
+                };
+                next->content = string_trim(&next->content, 1, next->content.length);
+            } else {
+                // TODO: No operator error
+                return false;
+            }
+        }
+    }
+
+    return true;
 }
 
 static bool is_digit(const uint8_t c)
--- parser.c
+++ parser.c
@@ -35,7 +35,6 @@ static void parser_backup(Parser* const parser);
 // static Token* parser_peek(Parser* const parser);
 
 static void print_expression_inorder(Expr* const expr, const bool verbose);
-static bool preprocess_tokens(List* const tokens);
 
 Tree parse_expression(List* const tokens)
 {
@@ -45,8 +44,6 @@ Tree parse_expression(List* const tokens)
         .input = *tokens,
         .expr = Tree(),
     };
-
-    preprocess_tokens(tokens); 
 
     parser_parse(&parser);
 
@@ -229,40 +226,4 @@ static void print_expression_inorder(Expr* const expr, const bool verbose)
 
     if (!single && verbose)
         putc(')', stdout);
-}
-
-// When two numbers are together and the second number contains an operator
-// at the beginning of it, then input was {"X", "+Y"} or {"X", "-Y"}, thus
-// an operator token must be synthesised from it and operator symbol cut afterwards
-static bool preprocess_tokens(List* const tokens)
-{
-    assert(tokens != NULL);
-
-    for list_range(it, *tokens) {
-        Token* token = list_node_data(it, Token);
-        ListNodeBase* next_it = it->next;
-
-        if (next_it == NULL)
-            break;
-
-        Token* next = list_node_data(next_it, Token);
-
-        if (token->type == TokenType_number &&
-            next->type == TokenType_number) {
-            const uint8_t op = next->content.text[0];
-
-            if (op == '+' || op == '-') {
-                *list_insert_after(tokens, it, Token) = (Token){
-                    .type = op == '+' ? TokenType_add : TokenType_subtract,
-                    .content = string_trim(&next->content, 0, 1),
-                };
-                next->content = string_trim(&next->content, 1, next->content.length);
-            } else {
-                // TODO: No operator error
-                return false;
-            }
-        }
-    }
-
-    return true;
 }
