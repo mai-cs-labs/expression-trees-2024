5e32f32fc71d652fc6d36016ae165056b5f96a9f
vladeemerr <50291143+vladeemerr@users.noreply.github.com>
2024-04-20 23:32:29 +0300

    Replace reading input from stdin with file

--- main.c
+++ main.c
@@ -12,158 +12,146 @@
 
 static void print_short_usage(void)
 {
-    LOG("Usage: expr [-h|--help] [-v] [<command>] {expression}\n");
-    exit(EXIT_SUCCESS);
+	LOG("Usage: expr [-h|--help] [-v] [-f <file>] [<command>] {expression}\n");
+	exit(EXIT_SUCCESS);
 }
 
 static void print_long_usage(void)
 {
-    LOG("Usage: expr [-h|--help] [-v] [<command>] {expression}\n\n"
-        "\t-h, --help\n"
-        "\t\tOutput a usage message and exit\n\n"
-        "\t-v\n"
-        "\t\tEnable verbose expression output\n\n"
-        "\tcommand, any of:\n"
-        "\t\tsimplify\tsimplify resulting expression (default)\n"
-        "\t\texpand\t\texpand resulting expression\n"
-        "\t\teval\t\tevaluate resulting expression\n\n");
-    exit(EXIT_SUCCESS);
-}
-
-static String read_stdin(void)
-{
-    uint8_t* data = malloc(128);
-    if (data == NULL)
-        return (String){NULL, 0};
-
-    size_t offset = 0;
-    size_t size;
-
-    for (;;) {
-        size = fread(data + offset, 1, 128, stdin);
-        if (size == (size_t)EOF)
-            break;
-
-        offset += size;
-        if (size == 128)
-            data = realloc(data, offset + size);
-        else
-            break;
-    }
-
-    if (offset > 0)
-        --offset;
-
-    if (offset == 0) {
-        free(data);
-        data = NULL;
-    }
-
-    return (String){data, offset};
+	LOG("Usage: expr [-h|--help] [-v] [<command>] {expression}\n\n"
+		"\t-h, --help\n"
+		"\t\tOutput a usage message and exit\n\n"
+		"\t-v\n"
+		"\t\tEnable verbose expression output\n\n"
+		"\t-f <file>\n"
+		"\t\tRead expression from file\n\n"
+		"\tcommand, any of:\n"
+		"\t\tsimplify\tsimplify resulting expression (default)\n"
+		"\t\texpand\t\texpand resulting expression\n"
+		"\t\teval\t\tevaluate resulting expression\n\n");
+	exit(EXIT_SUCCESS);
 }
 
 static void print_expression(const Expression* const expression, const bool verbose)
 {
-    assert(expression != NULL);
+	assert(expression != NULL);
 
-    if (verbose)
-        expression_verbose_print(expression);
-    else
-        expression_print(expression);
+	if (verbose)
+		expression_verbose_print(expression);
+	else
+		expression_print(expression);
 }
 
 int main(int argc, char* argv[])
 {
-    int result = EXIT_SUCCESS;
-    bool verbose = false;
-    TransformMode transform = TransformMode_Simplify;
+	int result = EXIT_SUCCESS;
+	bool verbose = false;
+	TransformMode transform = TransformMode_Simplify;
 
-    String input;
+	String input;
 
-    int argp = 1;
-    if (argc == 1) {
-        input = read_stdin();
-        if (string_empty(&input))
-            print_short_usage();
-    }
-    else {
-        while (argp != argc) {
-            if (strcmp(argv[argp], "-v") == 0) {
-                verbose = true;
-                ++argp;
-            }
-            else if (strcmp(argv[argp], "-h") == 0)
-                print_short_usage();
-            else if (strcmp(argv[argp], "--help") == 0)
-                print_long_usage();
-            else if (strcmp(argv[argp], "simplify") == 0) {
-                transform = TransformMode_Simplify;
-                ++argp;
-            }
-            else if (strcmp(argv[argp], "expand") == 0) {
-                transform = TransformMode_Expand;
-                ++argp;
-            }
-            else if (strcmp(argv[argp], "eval") == 0) {
-                transform = TransformMode_Evaluate;
-                ++argp;
-            }
-            else
-                break;
-        }
+	int argp = 1;
+	char* filename = NULL;
 
-        if (argv[argp] == NULL) {
-            input = read_stdin();
-            if (string_empty(&input))
-                print_short_usage();
-        }
-        else
-            input = string_init(argv[argp]);
-    }
+	if (argc > 1) {
+		while (argp != argc) {
+			if (strcmp(argv[argp], "-v") == 0) {
+				verbose = true;
+				++argp;
+			}
+			else if (strcmp(argv[argp], "-h") == 0)
+				print_short_usage();
+			else if (strcmp(argv[argp], "--help") == 0)
+				print_long_usage();
+			else if (strcmp(argv[argp], "-f") == 0) {
+				filename = argv[argp + 1];
+				argp += 2;
+			}
+			else if (strcmp(argv[argp], "simplify") == 0) {
+				transform = TransformMode_Simplify;
+				++argp;
+			}
+			else if (strcmp(argv[argp], "expand") == 0) {
+				transform = TransformMode_Expand;
+				++argp;
+			}
+			else if (strcmp(argv[argp], "eval") == 0) {
+				transform = TransformMode_Evaluate;
+				++argp;
+			}
+			else
+				break;
+		}
+	}
+	else
+		print_short_usage();
 
-    List tokens = lexical_scan(&input);
+	if (filename != NULL) { // @NOTE: Expression provided as file
+		FILE* const file = fopen(filename, "r");
+		if (file == NULL) {
+			LOGF("Failed to open file %s\n", filename);
+			return EXIT_FAILURE;
+		}
 
-    if (check_illegal_tokens(&tokens)) {
-        result = EXIT_FAILURE;
-        goto error_scan;
-    }
+		fseek(file, 0, SEEK_END);
+		input = string_create(ftell(file));
+		rewind(file);
 
-    if (verbose)
-        debug_print_tokens(&tokens);
+		if (fread(input.text, sizeof(uint8_t), input.length, file) != input.length) {
+			LOGF("Failed to read file %s\n", filename);
+			string_destroy(&input);
+			fclose(file);
+			return EXIT_FAILURE;
+		}
 
-    Expression* expression = expression_parse(&tokens);
-    if (expression == NULL) {
-        result = EXIT_FAILURE;
-        goto error_scan;
-    }
+		fclose(file);
+	}
+	else if (argv[argp] != NULL) // @NOTE: Expression provided as argument
+		input = string_init(argv[argp]);
+	else
+		print_short_usage();
 
-    if (expression_empty(expression))
-        goto cleanup;
+	List tokens = lexical_scan(&input);
 
-    switch (transform) {
-    case TransformMode_Simplify:
-        simplify_expression(expression);
-        print_expression(expression, verbose);
-        break;
+	if (check_illegal_tokens(&tokens)) {
+		result = EXIT_FAILURE;
+		goto error_scan;
+	}
 
-    case TransformMode_Expand:
-        expand_expression(expression);
-        print_expression(expression, verbose);
-        break;
+	if (verbose)
+		debug_print_tokens(&tokens);
 
-    case TransformMode_Evaluate:
-        printf("%.12g\n", evaluate_expression(expression));
-        break;
-    }
+	Expression* expression = expression_parse(&tokens);
+	if (expression == NULL) {
+		result = EXIT_FAILURE;
+		goto error_scan;
+	}
+
+	if (expression_empty(expression))
+		goto cleanup;
+
+	switch (transform) {
+	case TransformMode_Simplify:
+		simplify_expression(expression);
+		print_expression(expression, verbose);
+		break;
+
+	case TransformMode_Expand:
+		expand_expression(expression);
+		print_expression(expression, verbose);
+		break;
+
+	case TransformMode_Evaluate:
+		printf("%.12g\n", evaluate_expression(expression));
+		break;
+	}
 
 cleanup:
-    expression_destroy(&expression);
+	expression_destroy(&expression);
 error_scan:
-    list_deinit(&tokens);
+	list_deinit(&tokens);
 
-    // @TODO: Sane way of checking allocated string
-    if (input.text != (uint8_t*)argv[argp])
-        free(input.text);
+	string_destroy(&input);
 
-    return result;
+	return result;
 }
