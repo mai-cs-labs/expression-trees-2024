64e68a120de37c77eb04781ddbd0ab9dcb38a1eb
dimukhametovan <girls8560@yandex.ru>
2024-04-28 18:33:59 +0300

    mesto dlya vashei reklami

--- transform.c
+++ transform.c
@@ -3,30 +3,33 @@
 #include <assert.h>
 #include <float.h>
 #include <math.h>
+#include <string.h>
 
 #include "lexer.h"
 
 // @NOTE: Put transformer functions prototypes here
-static bool factor_difference_of_squares(Expression* const expression);
-static bool fold_multipliers_to_diff_of_squares(Expression* const expression);
+static bool factor_difference_of_squares(Expression *const expression);
+static bool fold_multipliers_to_diff_of_squares(Expression *const expression);
+static bool order_adjacent_operands(Expression *const expression);
 
 // Make deep copy of a given expression.
-static Expression* expression_copy(const Expression* const expression);
+static Expression *expression_copy(const Expression *const expression);
 
 // Compare two expressions, lhs and rhs and return true if two
 // expressions are identical, otherwise - false.
-static bool expression_equal(const Expression* const lhs,
-                             const Expression* const rhs);
+static bool expression_equal(const Expression *const lhs,
+							 const Expression *const rhs);
 
-void simplify_expression(Expression* const expression)
+void simplify_expression(Expression *const expression)
 {
 	assert(expression != NULL);
 
 	// @NOTE: Put simplification transformer functions here
+	order_adjacent_operands(expression);
 	fold_multipliers_to_diff_of_squares(expression);
 }
 
-void expand_expression(Expression* const expression)
+void expand_expression(Expression *const expression)
 {
 	assert(expression != NULL);
 
@@ -34,56 +37,148 @@ void expand_expression(Expression* const expression)
 	factor_difference_of_squares(expression);
 }
 
-double evaluate_expression(const Expression* const expression)
+double evaluate_expression(const Expression *const expression)
 {
 	assert(expression != NULL);
 
-	switch (expression->type) {
-	case ExpressionType_Literal: {
-		Literal* const literal = (Literal*)expression;
+	switch (expression->type)
+	{
+	case ExpressionType_Literal:
+	{
+		Literal *const literal = (Literal *)expression;
 
 		if (literal->tag == LiteralTag_Number)
 			return literal->number;
-	} break;
+	}
+	break;
 
-	case ExpressionType_Unary: {
-		UnaryExpression* const unary = (UnaryExpression*)expression;
+	case ExpressionType_Unary:
+	{
+		UnaryExpression *const unary = (UnaryExpression *)expression;
 
-		if (unary->operator == TokenType_Minus)
+		if (unary->operator== TokenType_Minus)
 			return -evaluate_expression(unary->subexpression);
 
 		return evaluate_expression(unary->subexpression);
-	} break;
+	}
+	break;
 
-	case ExpressionType_Binary: {
-		BinaryExpression* const binary = (BinaryExpression*)expression;
+	case ExpressionType_Binary:
+	{
+		BinaryExpression *const binary = (BinaryExpression *)expression;
 
-		switch (binary->operator) {
+		switch (binary->operator)
+		{
 		case TokenType_Plus:
 			return evaluate_expression(binary->left) +
-			       evaluate_expression(binary->right);
+				   evaluate_expression(binary->right);
 
 		case TokenType_Minus:
 			return evaluate_expression(binary->left) -
-			       evaluate_expression(binary->right);
+				   evaluate_expression(binary->right);
 
 		case TokenType_Multiply:
 			return evaluate_expression(binary->left) *
-			       evaluate_expression(binary->right);
+				   evaluate_expression(binary->right);
 
 		case TokenType_Divide:
 			return evaluate_expression(binary->left) /
-			       evaluate_expression(binary->right);
+				   evaluate_expression(binary->right);
 
 		case TokenType_Exponent:
 			return pow(evaluate_expression(binary->left),
-			           evaluate_expression(binary->right));
+					   evaluate_expression(binary->right));
 		}
-	} break;
+	}
+	break;
 	}
 
 	// @NOTE: ExpressionType_Empty and LiteralTag_Symbol case
 	return 0;
+}
+static bool expression_lexicographic_compare(const Expression *const exp1, const Expression *const exp2)
+{
+	// Проверяем, что указатели не NULL
+	// if (exp1 == NULL || exp2 == NULL)
+	// {
+	// 	return false; // Можно также вернуть -1 или 1, в зависимости от требований
+	// }
+
+	// Преобразуем указатели в строки для сравнения
+	Literal *const lhs_literal = (Literal *)exp1;
+	Literal *const rhs_literal = (Literal *)exp2;
+
+	// Сравниваем строки лексикографически
+	if (strcmp((char *)&lhs_literal->symbol, (char *)&rhs_literal->symbol) < 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+static bool order_adjacent_operands(Expression *const expression)
+{
+	// выражение не нулевое
+	assert(expression != NULL);
+	bool changed = false;
+	// проверка типа выражения
+	switch (expression->type)
+	{
+		// если унарное, то указывает самое на себя
+	case ExpressionType_Unary:
+	{
+		UnaryExpression *const unary = (UnaryExpression *)expression;
+		changed |= order_adjacent_operands(unary->subexpression);
+	}
+	break;
+		// если бинарное
+	case ExpressionType_Binary:
+	{
+		// создаем указатель
+		BinaryExpression *const binary = (BinaryExpression *)expression;
+		const bool other_result = order_adjacent_operands(binary->left) |
+								  order_adjacent_operands(binary->right);
+
+		// проверка сложение или умножение
+		if (binary->operator== TokenType_Plus || binary->operator== TokenType_Multiply)
+		{
+			// является ли левый и правый операнды бинарным выражением
+			if (binary->left->type == ExpressionType_Binary)
+			{
+				changed |= order_adjacent_operands(binary->left);
+			}
+			if (binary->right->type == ExpressionType_Binary)
+			{
+				changed |= order_adjacent_operands(binary->right);
+			}
+			// если да, то вызов рекурсивных функций
+			Literal *const lhs_literal = (Literal *)binary->left;
+			Literal *const rhs_literal = (Literal *)binary->right;
+			// Literal *const number = (Literal *)rhs->right;
+			if ((lhs_literal->tag == LiteralTag_Symbol) && (rhs_literal->tag == LiteralTag_Symbol))
+			{
+				if (expression_lexicographic_compare(binary->left, binary->right) == false)
+				{
+					Expression *temp = binary->left;
+					binary->left = binary->right;
+					binary->right = temp;
+					changed = true;
+					printf("change");
+				}
+			}
+			return true;
+			// проверка лексикографического порядка
+		}
+		else
+		{
+			return other_result;
+		}
+	}
+	break;
+	}
+	return false;
 }
 
 //
@@ -122,45 +217,55 @@ double evaluate_expression(const Expression* const expression)
   A   B A   B
 #endif
 // @NOTE: If statements were not flattened for relative clarity
-static bool factor_difference_of_squares(Expression* const expression)
+static bool factor_difference_of_squares(Expression *const expression)
 {
 	assert(expression != NULL);
 
-	switch (expression->type) {
+	switch (expression->type)
+	{
 	// Try to find difference of squares in unary expression
-	case ExpressionType_Unary: {
-		UnaryExpression* const unary = (UnaryExpression*)expression;
+	case ExpressionType_Unary:
+	{
+		UnaryExpression *const unary = (UnaryExpression *)expression;
 		return factor_difference_of_squares(unary->subexpression);
-	} break;
+	}
+	break;
 
-	case ExpressionType_Binary: {
-		BinaryExpression* const binary = (BinaryExpression*)expression;
+	case ExpressionType_Binary:
+	{
+		BinaryExpression *const binary = (BinaryExpression *)expression;
 
 		// Try to find difference of squares in the sides of binary expression first
 		const bool other_result = factor_difference_of_squares(binary->left) |
-		                          factor_difference_of_squares(binary->right);
+								  factor_difference_of_squares(binary->right);
 
 		// First step, find _difference_ of two expressions
-		if (binary->operator == TokenType_Minus) {
-			Expression* a = NULL;
-			Expression* b = NULL;
+		if (binary->operator== TokenType_Minus)
+		{
+			Expression *a = NULL;
+			Expression *b = NULL;
 			bool lhs_pow_of_two = false;
 			bool rhs_pow_of_two = false;
 
 			// Left subexpression must be a binary operation
-			if (binary->left->type == ExpressionType_Binary) {
-				BinaryExpression* const lhs = (BinaryExpression*)binary->left;
+			if (binary->left->type == ExpressionType_Binary)
+			{
+				BinaryExpression *const lhs = (BinaryExpression *)binary->left;
 
 				// and operator must be "raise to power of"
-				if (lhs->operator == TokenType_Exponent) {
+				if (lhs->operator== TokenType_Exponent)
+				{
 					// exponent must be a literal
-					if (lhs->right->type == ExpressionType_Literal) {
-						Literal* const number = (Literal*)lhs->right;
+					if (lhs->right->type == ExpressionType_Literal)
+					{
+						Literal *const number = (Literal *)lhs->right;
 
 						// specifically, number literal
-						if (number->tag == LiteralTag_Number) {
+						if (number->tag == LiteralTag_Number)
+						{
 							// specifically, with value of 2
-							if (fabs(number->number - 2.0) < DBL_EPSILON) {
+							if (fabs(number->number - 2.0) < DBL_EPSILON)
+							{
 								lhs_pow_of_two = true;
 								a = lhs->left;
 							}
@@ -170,14 +275,19 @@ static bool factor_difference_of_squares(Expression* const expression)
 			}
 
 			// Same as above, but for the right subexpression
-			if (binary->right->type == ExpressionType_Binary) {
-				BinaryExpression* const rhs = (BinaryExpression*)binary->right;
+			if (binary->right->type == ExpressionType_Binary)
+			{
+				BinaryExpression *const rhs = (BinaryExpression *)binary->right;
 
-				if (rhs->operator == TokenType_Exponent) {
-					if (rhs->right->type == ExpressionType_Literal) {
-						Literal* const number = (Literal*)rhs->right;
-						if (number->tag == LiteralTag_Number) {
-							if (fabs(number->number - 2.0) < DBL_EPSILON) {
+				if (rhs->operator== TokenType_Exponent)
+				{
+					if (rhs->right->type == ExpressionType_Literal)
+					{
+						Literal *const number = (Literal *)rhs->right;
+						if (number->tag == LiteralTag_Number)
+						{
+							if (fabs(number->number - 2.0) < DBL_EPSILON)
+							{
 								rhs_pow_of_two = true;
 								b = rhs->left;
 							}
@@ -188,22 +298,23 @@ static bool factor_difference_of_squares(Expression* const expression)
 
 			// This is true when left and right differences
 			// are expressions "raise to power of two" respectively
-			if (lhs_pow_of_two && rhs_pow_of_two) {
+			if (lhs_pow_of_two && rhs_pow_of_two)
+			{
 				// Change operator from subtraction to multiplication
-				binary->operator = TokenType_Multiply;
+				binary->operator= TokenType_Multiply;
 
 				// Remove parentheses from factors
 				a->parenthesised = false;
 				b->parenthesised = false;
 
 				// Create new (A - B) expression, notice the copy
-				BinaryExpression* new_lhs = expression_binary_create(
+				BinaryExpression *new_lhs = expression_binary_create(
 					TokenType_Minus, expression_copy(a), expression_copy(b));
 				// A - B subexpression must be parenthesised
 				new_lhs->base.parenthesised = true;
 
 				// Create new (A + B) expression, notice the copy
-				BinaryExpression* new_rhs = expression_binary_create(
+				BinaryExpression *new_rhs = expression_binary_create(
 					TokenType_Plus, expression_copy(a), expression_copy(b));
 				// A + B subexpression must be parenthesised
 				new_rhs->base.parenthesised = true;
@@ -213,8 +324,8 @@ static bool factor_difference_of_squares(Expression* const expression)
 				expression_destroy(&binary->right);
 
 				// Assign the difference expression new left and right subexpressions
-				binary->left = (Expression*)new_lhs;
-				binary->right = (Expression*)new_rhs;
+				binary->left = (Expression *)new_lhs;
+				binary->right = (Expression *)new_rhs;
 
 				return true;
 			}
@@ -222,7 +333,8 @@ static bool factor_difference_of_squares(Expression* const expression)
 
 		// Maybe the differences of squares were found deeper in the expression?
 		return other_result;
-	} break;
+	}
+	break;
 	}
 
 	// Not found
@@ -235,41 +347,48 @@ static bool factor_difference_of_squares(Expression* const expression)
 // Examples: (a - b) * (a + b) -> a^2 - b^2,
 //           (a - ((c - d) * (c + d))) * (a + ((c - d) * (c + d))) ->
 //        -> a ^ 2 * (c ^ 2 * d ^ 2) ^ 2
-static bool fold_multipliers_to_diff_of_squares(Expression* const expression)
+static bool fold_multipliers_to_diff_of_squares(Expression *const expression)
 {
 	assert(expression != NULL);
 
-	switch (expression->type) {
-	case ExpressionType_Unary: {
-		UnaryExpression* const unary = (UnaryExpression*)expression;
+	switch (expression->type)
+	{
+	case ExpressionType_Unary:
+	{
+		UnaryExpression *const unary = (UnaryExpression *)expression;
 		return fold_multipliers_to_diff_of_squares(unary->subexpression);
-	} break;
+	}
+	break;
 
-	case ExpressionType_Binary: {
-		BinaryExpression* const binary = (BinaryExpression*)expression;
+	case ExpressionType_Binary:
+	{
+		BinaryExpression *const binary = (BinaryExpression *)expression;
 
 		const bool other_result = fold_multipliers_to_diff_of_squares(binary->left) |
-		                          fold_multipliers_to_diff_of_squares(binary->right);
+								  fold_multipliers_to_diff_of_squares(binary->right);
 
-		BinaryExpression* lhs = NULL;
-		BinaryExpression* rhs = NULL;
+		BinaryExpression *lhs = NULL;
+		BinaryExpression *rhs = NULL;
 
 		// Must be multiplication of factors
-		if (binary->operator != TokenType_Multiply)
+		if (binary->operator!= TokenType_Multiply)
 			return other_result;
 
 		// Factors must be binary operations
 		if (binary->left->type != ExpressionType_Binary ||
-		    binary->right->type != ExpressionType_Binary) {
+			binary->right->type != ExpressionType_Binary)
+		{
 			return other_result;
 		}
 
-		lhs = (BinaryExpression*)binary->left;
-		rhs = (BinaryExpression*)binary->right;
+		lhs = (BinaryExpression *)binary->left;
+		rhs = (BinaryExpression *)binary->right;
 
 		// Left factor must be a difference, right factor must be a sum
-		if (lhs->operator != TokenType_Minus ||
-		    rhs->operator != TokenType_Plus) {
+		if (lhs->operator!= TokenType_Minus ||
+			rhs->
+			operator!= TokenType_Plus)
+		{
 			return other_result;
 		}
 
@@ -279,32 +398,32 @@ static bool fold_multipliers_to_diff_of_squares(Expression* const expression)
 
 		// Subexpressions of factors must be the same
 		if (!expression_equal(lhs->left, rhs->left) ||
-		    !expression_equal(lhs->right, rhs->right)) {
+			!expression_equal(lhs->right, rhs->right))
+		{
 			return other_result;
 		}
 
-		binary->operator = TokenType_Minus;
+		binary->operator= TokenType_Minus;
 
-		BinaryExpression* const new_lhs = expression_binary_create(
+		BinaryExpression *const new_lhs = expression_binary_create(
 			TokenType_Exponent,
 			expression_copy(lhs->left),
-			(Expression*)expression_literal_create_number(2)
-		);
+			(Expression *)expression_literal_create_number(2));
 
-		BinaryExpression* const new_rhs = expression_binary_create(
+		BinaryExpression *const new_rhs = expression_binary_create(
 			TokenType_Exponent,
 			expression_copy(lhs->right),
-			(Expression*)expression_literal_create_number(2)
-		);
+			(Expression *)expression_literal_create_number(2));
 
-		expression_destroy((Expression**)&lhs);
-		expression_destroy((Expression**)&rhs);
+		expression_destroy((Expression **)&lhs);
+		expression_destroy((Expression **)&rhs);
 
-		binary->left = (Expression*)new_lhs;
-		binary->right = (Expression*)new_rhs;
+		binary->left = (Expression *)new_lhs;
+		binary->right = (Expression *)new_rhs;
 
 		return true;
-	} break;
+	}
+	break;
 	}
 
 	return false;
@@ -314,40 +433,48 @@ static bool fold_multipliers_to_diff_of_squares(Expression* const expression)
 // Helper functions
 //
 
-static Expression* expression_copy(const Expression* const expression)
+static Expression *expression_copy(const Expression *const expression)
 {
 	assert(expression != NULL);
 
-	Expression* result = NULL;
+	Expression *result = NULL;
 
-	switch (expression->type) {
-	case ExpressionType_Literal: {
-		Literal* const literal = (Literal*)expression;
+	switch (expression->type)
+	{
+	case ExpressionType_Literal:
+	{
+		Literal *const literal = (Literal *)expression;
 
-		switch (literal->tag) {
+		switch (literal->tag)
+		{
 		case LiteralTag_Number:
-			result = (Expression*)expression_literal_create_number(literal->number);
+			result = (Expression *)expression_literal_create_number(literal->number);
 			break;
 
 		case LiteralTag_Symbol:
-			result = (Expression*)expression_literal_create_symbol(&literal->symbol);
+			result = (Expression *)expression_literal_create_symbol(&literal->symbol);
 			break;
 		}
-	} break;
+	}
+	break;
 
-	case ExpressionType_Unary: {
-		UnaryExpression* const unary = (UnaryExpression*)expression;
-		result = (Expression*)expression_unary_create(
+	case ExpressionType_Unary:
+	{
+		UnaryExpression *const unary = (UnaryExpression *)expression;
+		result = (Expression *)expression_unary_create(
 			unary->operator, expression_copy(unary->subexpression));
-	} break;
+	}
+	break;
 
-	case ExpressionType_Binary: {
-		BinaryExpression* const binary = (BinaryExpression*)expression;
-		result = (Expression*)expression_binary_create(
+	case ExpressionType_Binary:
+	{
+		BinaryExpression *const binary = (BinaryExpression *)expression;
+		result = (Expression *)expression_binary_create(
 			binary->operator,
 			expression_copy(binary->left),
 			expression_copy(binary->right));
-	} break;
+	}
+	break;
 	}
 
 	result->parenthesised = expression->parenthesised;
@@ -355,8 +482,8 @@ static Expression* expression_copy(const Expression* const expression)
 	return result;
 }
 
-static bool expression_equal(const Expression* const lhs,
-                             const Expression* const rhs)
+static bool expression_equal(const Expression *const lhs,
+							 const Expression *const rhs)
 {
 	assert(lhs != NULL);
 	assert(rhs != NULL);
@@ -364,15 +491,18 @@ static bool expression_equal(const Expression* const lhs,
 	if (lhs->type != rhs->type)
 		return false;
 
-	switch (lhs->type) {
-	case ExpressionType_Literal: {
-		Literal* const lhs_literal = (Literal*)lhs;
-		Literal* const rhs_literal = (Literal*)rhs;
+	switch (lhs->type)
+	{
+	case ExpressionType_Literal:
+	{
+		Literal *const lhs_literal = (Literal *)lhs;
+		Literal *const rhs_literal = (Literal *)rhs;
 
 		if (lhs_literal->tag != rhs_literal->tag)
 			return false;
 
-		switch (lhs_literal->tag) {
+		switch (lhs_literal->tag)
+		{
 		case LiteralTag_Number:
 			if (fabs(lhs_literal->number - rhs_literal->number) < DBL_EPSILON)
 				return true;
@@ -383,30 +513,34 @@ static bool expression_equal(const Expression* const lhs,
 				return true;
 			break;
 		}
-	} break;
+	}
+	break;
 
-	case ExpressionType_Unary: {
-		UnaryExpression* const lhs_unary = (UnaryExpression*)lhs;
-		UnaryExpression* const rhs_unary = (UnaryExpression*)rhs;
+	case ExpressionType_Unary:
+	{
+		UnaryExpression *const lhs_unary = (UnaryExpression *)lhs;
+		UnaryExpression *const rhs_unary = (UnaryExpression *)rhs;
 
-		if (lhs_unary->operator != rhs_unary->operator)
+		if (lhs_unary->operator!= rhs_unary->operator)
 			return false;
 
 		return expression_equal(lhs_unary->subexpression, rhs_unary->subexpression);
-	} break;
+	}
+	break;
 
-	case ExpressionType_Binary: {
-		BinaryExpression* const lhs_binary = (BinaryExpression*)lhs;
-		BinaryExpression* const rhs_binary = (BinaryExpression*)rhs;
+	case ExpressionType_Binary:
+	{
+		BinaryExpression *const lhs_binary = (BinaryExpression *)lhs;
+		BinaryExpression *const rhs_binary = (BinaryExpression *)rhs;
 
-		if (lhs_binary->operator != rhs_binary->operator)
+		if (lhs_binary->operator!= rhs_binary->operator)
 			return false;
 
 		return expression_equal(lhs_binary->left, rhs_binary->left) &&
-		       expression_equal(lhs_binary->right, rhs_binary->right);
-	} break;
+			   expression_equal(lhs_binary->right, rhs_binary->right);
+	}
+	break;
 	}
 
 	return false;
 }
-
